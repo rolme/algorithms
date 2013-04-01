f4390d6725d4b6c953b115f91f4c7fd2beb0ee8d
rolme <roland.parnaso@gmail.com>
2013-04-01 11:36:25 -0700

    initial commit

---
+++ factorial.rb
@@ -0,0 +1,18 @@
+#!/Users/rparnaso/.rvm/rubies/ruby-2.0.0-p0/bin/ruby
+
+def factorial(n)
+  throw ArgumentError, "Argument cannot be a negative number" if (n < 0)
+  return 1 if (n <= 1)
+  return n * factorial(n-1)
+end
+
+if $0 == __FILE__
+  [4, 3, 2, 1, 0].each do |n|
+    x = factorial n
+    puts "#{n}! = #{x}"
+  end
+  
+  n = rand(5..25)
+  x = factorial n
+  puts "#{n}! = #{x}"
+end
---
+++ quiksort.rb
@@ -0,0 +1,80 @@
+#!/Users/rparnaso/.rvm/rubies/ruby-2.0.0-p0/bin/ruby
+
+# simple quick sort
+def quicksort(args)
+  list = args[:list].clone
+  return list if list.length <= 1
+  
+  pivot = rand(list.size)
+  pivot_value = list.delete_at(pivot)
+  less = Array.new
+  more = Array.new
+  list.each do |x|
+    if x <= pivot_value
+      less.push x
+    else
+      more.push x
+    end
+  end
+  return  quicksort(list: less) + [pivot_value] + quicksort(list: more)
+end
+
+if $0 == __FILE__
+  puts "QUICKSORT - SIMPLE"
+  unsorted = (0..99).to_a.sample(20)
+  puts "unsorted: #{unsorted}"
+  sorted = quicksort(list: unsorted)
+  puts "sorted:   #{sorted}"
+end
+
+
+class Array
+  def swap!(a,b)
+    self[a], self[b] = self[b], self[a]
+    self
+  end
+end
+
+def partition (args)
+  list = args[:list]
+  left = args[:left]
+  right = args[:right]
+  pivot = args[:pivot]
+  
+  pivot_value = list[pivot]
+  list.swap!(pivot, right)
+  index = left
+  
+  for i in left..right-1 do
+    if list[i] < pivot_value
+      list.swap!(i, index)
+      index += 1
+    end
+  end
+  list.swap!(index, right)
+  return index
+end
+
+# inline quick sort
+def quicksort(args)
+  list = args[:list]
+  left = args[:left]
+  right = args[:right]
+  
+  if left < right
+    pivot = rand(left..right)
+    new_pivot = partition(list: list, left: left, right: right, pivot: pivot)
+    quicksort(list: list, left: left, right: new_pivot - 1)
+    quicksort(list: list, left: new_pivot + 1, right: right)
+  end
+  list
+end
+
+if $0 == __FILE__
+  puts "QUICKSORT - IN LINE"
+  puts "unsorted: #{unsorted}"
+  sorted = quicksort(list: unsorted, left: 0, right: unsorted.size - 1)
+  puts "sorted:   #{sorted}"
+end
+
+
