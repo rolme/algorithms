6e64cd7610a187985e7187563b3f743cd22f7b9e
Roland Parnaso <roland.parnaso@gmail.com>
2013-05-18 13:54:16 -0700

    Sample string compression.

---
+++ strings/compression.rb
@@ -0,0 +1,63 @@
+# Given a string, compression it based on repeated characters.  If the
+# compression is longer, return the original string instead.
+#
+# NOTES:
+#   Avoid using String + String as this creates a new String
+#   Avoid "#{String}#{String}" as this is slow as well
+#   Use String << String to append to a String
+#   More information here: goo.gl/fPTmy
+class String
+  def compress(type=:repeated_characters)
+    self.send(type)
+  end
+
+private
+
+  def repeated_characters
+    return self if self.send(:_repeated_characters_size) >= self.size
+
+    compressed = ''
+    count = 0
+    previous = self.chr
+    self.chars.each do |chr|
+      if previous == chr
+        count += 1
+      else
+        compressed << previous << count.to_s
+        previous = chr
+        count = 1
+      end
+    end
+    compressed << previous << count.to_s
+  end
+
+  def _repeated_characters_size
+    size  = 0
+    count = 0
+    previous = self.chr
+    self.chars.each do |chr|
+      if previous == chr
+        count += 1
+      else
+        size += 1 + count.to_s.size
+        previous = chr
+        count = 1
+      end
+    end
+    size += 1 + count.to_s.size
+  end
+end
+
+if $0 == __FILE__
+  puts "Compress the follwing strings:"
+  puts
+  %w{
+    testimony
+    coding
+    boo
+    aabcdddddabbbdd
+    google
+  }.each do |word|
+    puts "'#{word}' compressed to: #{word.compress}"
+  end
+end
