a02a7f0635d602daf718b564f332996dcb93b7a2
Roland Parnaso <roland.parnaso@gmail.com>
2013-05-18 14:25:39 -0700

    Doing some clean up.
    - Remove unneeded return
    - Remove unneeded spaces
    - Remove unneeded parens
    - Use String#<< for appending
    - Use Array#sample instead of rand

--- math/factorial.rb
+++ math/factorial.rb
@@ -1,16 +1,14 @@
 def factorial(n)
   raise ArgumentError, "Argument cannot be a negative number" if (n < 0)
-  return 1 if (n <= 1)
-  return n * factorial(n-1)
+  return 1 if n <= 1
+  n * factorial(n - 1)
 end
 
 if $0 == __FILE__
   [4, 3, 2, 1, 0].each do |n|
-    x = factorial n
-    puts "#{n}! = #{x}"
+    puts n.to_s << "! = " << factorial(n).to_s
   end
-  
-  n = rand(5..25)
-  x = factorial n
-  puts "#{n}! = #{x}"
+
+  n = (5..25).to_a.sample
+  puts n.to_s << "! = " << factorial(n).to_s
 end
