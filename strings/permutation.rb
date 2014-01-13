6b1cf441f96f65c849fcd7240e1e96894b29acdb
rolme <roland.parnaso@gmail.com>
2014-01-12 19:42:05 -0800

    Added implementation of permutation. And better solution to problem.
    Question is determine if string is permutation of given word. Best to
    just check string length, sort, then compare.

--- strings/permutation.rb
+++ strings/permutation.rb
@@ -1,20 +1,69 @@
+class Permutation
+  def initialize(str)
+    @str_list = str.split('')
+  end
+
+  # performance      O(n^2)
+  # space complexity O(log n)
+  # Bad solution as I am looking for every permutation.
+  def is_permutation_of?(word)
+    permutations.include? word
+  end
+
+  def permutations
+    return @_permutations if @_permutations
+
+    list = @str_list
+    @_permutations = list.count < 2 ? list : permutate(list).collect(&:join)
+  end
+
+private
+
+  def permutate(list)
+    return [[list[0],list[1]], [list[1],list[0]]] if list.length == 2
+
+    item = list.slice!(0)
+    perms = permutate(list)
+    return add_permutations perms, item
+  end
+
+  def add_permutations(list, c)
+    new_list = []
+    list.each do |p|
+      length = p.length
+      while length >= 0
+        copy = p.clone
+        new_list << copy.insert(length, c)
+        length -= 1
+      end
+    end
+    new_list
+  end
+end
+
 # check if a given string is a permutation of another. Or in other words
 # an anagram of each other.
 class String
+  # performance      - O(n)
+  # space complexity - O(n)
+  # Best solution as a permutation will have the same # of sorted characters
   def is_permutation_of?(permutation)
-    permutation_list = self.permutations
-    permutation_list.include? permutation
+    return false if self.size != permutation.size
+    self.chars.sort == permutation.chars.sort
   end
 
   def permutations
     chars = self.chars
     permutations_list = chars.permutation
-    permutations_list.map{ |p| p.join }
+    permutations_list.collect(&:join)
   end
 
+  # performance      - O(n)
+  # space complexity - O(n)
+  # Ignore case and spaces.
   def is_anagram_of?(anagram)
-    self_frequency    = self.character_frequency
-    anagram_frequency = anagram.character_frequency
+    self_frequency    = self.downcase.delete(' ').character_frequency
+    anagram_frequency = anagram.downcase.delete(' ').character_frequency
     self_frequency.eql? anagram_frequency
   end
 
@@ -27,14 +76,13 @@ end
 if $0 == __FILE__
   word = 'mat'
   puts "Check following words are a permutation of '#{word}'"
+  puts "permutations of [#{word}]: #{word.permutations.count}: #{word.permutations.join(', ')}"
+  puts "permutations of [#{word}]: #{Permutation.new(word).permutations.count}: #{Permutation.new(word).permutations.join(', ')}"
   puts
-  %w{
-    atm
-    mat
-    mtm
-    tam
-  }.each do |permutation|
-    puts "'#{permutation}' permutation of [#{word}]: #{word.is_permutation_of? permutation}"
+  ['atm', 'mAt', 'a tm', 'tam'].each do |permutation|
+    puts "'#{permutation}' permutation of [#{word}] (Permutation): #{Permutation.new(word).is_permutation_of? permutation}"
+    puts "'#{permutation}' permutation of [#{word}] (String#permutation): #{word.is_permutation_of? permutation}"
     puts "'#{permutation}' anagram of [#{word}]: #{word.is_anagram_of? permutation}"
+    puts
   end
 end
